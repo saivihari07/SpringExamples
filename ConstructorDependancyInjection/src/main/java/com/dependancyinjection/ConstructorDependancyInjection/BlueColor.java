97c445ce644ad321caa5cd4ddfc89e9687c7bc9e
saivihari07 <100752661+saivihari07@users.noreply.github.com>
2022-06-16 12:12:44 +0530

    springEx

---
+++ ConstructorDependancyInjection/src/main/java/com/dependancyinjection/ConstructorDependancyInjection/BlueColor.java
@@ -0,0 +1,9 @@
+package com.dependancyinjection.ConstructorDependancyInjection;
+
+
+public class BlueColor implements MyColor 
+{
+	 public void printColor() {
+	        System.out.println("It is blue in color...");
+	    }
+}
